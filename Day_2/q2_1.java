0fe47361c9f37512a3aeabf2819c328df02fd3f7
arymishra07 <mishrabrothers068@gmail.com>
2026-05-29 22:45:40 +0530

    Add sum of digits program

--- Day_2/q2_1.java
+++ Day_2/q2_1.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class q2_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        int sum = 0;
+        int num = Math.abs(number);
+        
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        
+        System.out.println("Sum of digits: " + sum);
+        
+        scanner.close();
+    }
+}
