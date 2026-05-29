b9126036e9cc0f85e3f4b8f1de286c78344d4486
arymishra07 <mishrabrothers068@gmail.com>
2026-05-29 22:22:24 +0530

    Add program to find product of digits of a number

--- Day_2/q2_3.java
+++ Day_2/q2_3.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class q2_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        int product = 1;
+        int n = Math.abs(num);
+        
+        while (n > 0) {
+            product *= n % 10;
+            n /= 10;
+        }
+        
+        System.out.println("Product of digits: " + product);
+        sc.close();
+    }
+}
