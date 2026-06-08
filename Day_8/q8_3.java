a0e3e105f5e047d9a78b810f3e282a7310efd75b
arymishra07 <25112cn027@glbitm.ac.in>
2026-06-08 21:57:06 +0530

    this is day 8 task

---
+++ Day_8/q8_3.java
@@ -0,0 +1,13 @@
+package Day_8;
+public class q8_3 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (char ch = 'A'; ch < 'A' + i; ch++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
