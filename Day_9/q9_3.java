918fd9599f1af4dd107f6dc10611b2463963d34f
arymishra07 <25112cn027@glbitm.ac.in>
2026-06-09 22:44:41 +0530

    this is day 8

---
+++ Day_9/q9_3.java
@@ -0,0 +1,12 @@
+package Day_9;
+public class q9_3 {
+ public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            char ch = (char) ('A' + i - 1);
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
