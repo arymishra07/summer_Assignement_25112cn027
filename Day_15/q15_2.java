f2ca35371290ad1c58fa7f732007d8305395c3cb
arymishra07 <25112cn027@glbitm.ac.in>
2026-06-15 21:12:47 +0530

    this is day 15

--- Day_15/q15_2.java
+++ Day_15/q15_2.java
@@ -1,5 +1,30 @@
 package Day_15;
 
-public class q15_2 {
+
     
+import java.util.Scanner;
+
+public class q15_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int first = arr[0];
+
+        for (int i = 0; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[n - 1] = first;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
--- Day_15/q15_3.java
+++ Day_15/q15_3.java
@@ -1,5 +1,30 @@
 package Day_15;
 
-public class q15_3 {
+
     
+import java.util.Scanner;
+
+public class q15_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int last = arr[n - 1];
+
+        for (int i = n - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = last;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
